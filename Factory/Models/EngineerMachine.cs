b3eed45c8f2a7c7c51809999f84bc46aea2c35fd
ilromali <iliashenkoroman@gmail.com>
2021-01-08 13:02:40 -0800

    Fix bugs

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -2,7 +2,7 @@ namespace Factory.Models
 {
   public class EngineerMachine
   {
-    public int? JoinId { get; set; }
+    public int? EngineerMachineId { get; set; }
     public int? EngineerId { get; set; }
     public int? MachineId { get; set; }
     public Engineer Engineer { get; set; }

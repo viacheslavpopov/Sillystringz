443bec9a2124784a0a5d02753420c15b28ed0522
ilromali <iliashenkoroman@gmail.com>
2021-01-08 13:45:19 -0800

    Add engineers create to views & controller

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,5 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,6 +20,23 @@ namespace Factory.Controllers
     {
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CourseId = new SelectList(_db.Engineers, "EngineerId", "FirstName", "LastName");
+      return View();
+    }
+
+    public ActionResult Create(Engineer engineer, int MachineId)
+    {
+      _db.Engineers.Add(engineer);
+      if (MachineId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }

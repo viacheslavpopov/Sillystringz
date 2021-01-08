081d19a831eb6d16636ccfd4c6add043821ea4f9
ilromali <iliashenkoroman@gmail.com>
2021-01-08 09:28:16 -0800

    Create Engineer class & properties

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.Machines = new HashSet<EngineerMachine>();
+    }
+    public int? EngineerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; } //xxx-xxx-xxxx format
+    public string Email { get; set; }
+    public virtual ICollection<EngineerMachine> Machines { get; set; }
+  }
+}

4d9297006d9f01192900cb761c04799a6db70138
ilromali <iliashenkoroman@gmail.com>
2021-01-08 09:36:06 -0800

    Create factory context class & properties

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DBContext
+  {
+    public virtual DbSet<Engineer> Engineers { get; set; }
+    public virtual DbSet<Machine> Machines { get; set; }
+    public DbSet<EngineerMachine> EngineerMachine { get; set; }
+    public FactoryContext(DbContextOptions options) : base(options) { }
+  }
+}
